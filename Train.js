438140e8625fc86898cb02fe41488f8f3e1eeb09
Haoyang Wei <weihaoyang@ainnovation.com>
2020-04-05 23:48:11 +0800

    finishing

--- Train.js
+++ Train.js
@@ -74,18 +74,14 @@ async function showConfusion(model, data) {
 }
 
 async function showExamples(data) {
-  // Create a container in the visor
   const surface =
     tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data' });
 
-  // Get the examples
   const examples = data.nextTestBatch(20);
   const numExamples = examples.xs.shape[0];
 
-  // Create a canvas element to render each example
   for (let i = 0; i < numExamples; i++) {
     const imageTensor = tf.tidy(() => {
-      // Reshape the image to 28x28 px
       return examples.xs
         .slice([i, 0], [1, examples.xs.shape[1]])
         .reshape([28, 28, 1]);
